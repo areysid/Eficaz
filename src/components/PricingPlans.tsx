0dc9acf33c34ff67aa2c0bdd68e513f5a9716b88
Sidharth Malpani <135989098+areysid@users.noreply.github.com>
2025-08-24 03:07:03 +0530

    Update PricingPlans.tsx

--- src/components/PricingPlans.tsx
+++ src/components/PricingPlans.tsx
@@ -202,7 +202,7 @@ const PricingPlans = () => {
       </main>
 
       {/* Footer */}
-      <footer className="border-t py-12 h-20 pb-10">
+      <footer className="border-t py-12 relative z-10">
         <div className="container text-center space-y-4">
           <img
             src="/lovable-uploads/8b1d9995-e628-4d9d-bd6e-ccc312b188ca.png"
