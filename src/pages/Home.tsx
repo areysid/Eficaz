7ed2e321f56e09526b09c004e5b7e007f751cb32
Sidharth Malpani <135989098+areysid@users.noreply.github.com>
2025-10-17 01:58:14 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -311,8 +311,10 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="flex justify-center">
-            <WebinarTimeline />
+          <div className="flex justify-center w-full sm:w-auto sm:justify-center px-4 sm:px-0">
+           <div className="w-full max-w-md sm:max-w-xl">
+             <WebinarTimeline />
+           </div>
           </div>
         </div>
       </motion.section>
